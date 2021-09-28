f28bdb5ce4747aa19c9fda4d4c12460b3b2ba747
hazman1yusof <hazman.yusof@gmail.com>
2021-09-28 17:01:59 +0800

    sdd

---
+++ webbridge_doctornote/diagnose.py
@@ -0,0 +1,41 @@
+import ConfigParser
+import mysql.connector
+import requests
+import json    
+import array
+
+db_conf = ConfigParser.RawConfigParser()
+
+array_all = ['diagnose','pathealth','pathistory','patexam']
+
+my_all = {}
+
+for ini_name in array_all:
+    
+    my_dict = {}
+    
+    db_conf.read(ini_name+'.ini')
+    configlist=db_conf.sections()
+
+    for section in configlist:
+        my_dict[section] = db_conf.items(section)
+
+    my_all[ini_name] = my_dict
+    
+
+# print my_all
+
+url = 'https://www.medicsoft.com.my/webbridge/public/diagnose'
+data = my_all
+header = {
+        'Accept' : 'application/json', 
+        'Content-Type' : 'application/json',
+        "Accept-Encoding": "gzip, deflate, sdch, br",
+        "Accept-Language": "en-US,en;q=0.8",
+        "User-Agent": "Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion",
+    }
+
+
+post = requests.post(url,data=json.dumps(data),headers = header).text
+
+print(post)
